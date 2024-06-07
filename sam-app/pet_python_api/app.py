4b32335cc1f3eefbd6987ef5445d2b3f692993e1
Kevin <kevinyenhaha@gmail.com>
2024-06-07 19:09:09 +1000

    Built a S3 config for image uploads with DynamoDB

--- sam-app/pet_python_api/app.py
+++ sam-app/pet_python_api/app.py
@@ -1,5 +1,8 @@
+import os
 import json
 import boto3
+import pybase64
+import uuid
 from datetime import datetime
 import graphene
 from graphene import ObjectType, String, Int, Boolean, Field, List, ID, InputObjectType
@@ -13,8 +16,12 @@ api_doc(app, config_path='./config/config.yaml', url_prefix='/api/doc', title='A
 if __name__ == '__main__':
     app.run()
 
+S3_BUCKET = os.environ.get('S3_BUCKET', 'pet-profile-image')
+DYNAMODB_TABLE_NAME = os.environ.get('DYNAMODB_TABLE', 'pet-and-paws-pet')
+
 dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
-table = dynamodb.Table('pet-and-paws-pet')
+table = dynamodb.Table(DYNAMODB_TABLE_NAME)
+s3_client = boto3.client('s3', region_name='ap-southeast-2')
 
 class PetInput(InputObjectType):
     petId = ID(required=True)
@@ -89,7 +96,20 @@ class CreatePetProfile(graphene.Mutation):
             raise Exception('Missing required fields')
         pet_profile_data_dict = petProfileData.__dict__
         pet_profile_data_dict['uploadDate'] = datetime.utcnow().isoformat()
+
+        print("Pet profile data:", pet_profile_data_dict)
+
+        image_data = pybase64.b64decode(pet_profile_data_dict.pop('image'))
+        image_id = str(uuid.uuid4())
+        s3_key = f"{image_id}.jpg"
+
+
         try:
+            print(f"Uploading image to S3: Bucket={S3_BUCKET}, Key={s3_key}")
+            s3_client.put_object(Bucket=S3_BUCKET, Key=s3_key, Body=image_data)
+            image_url = f"https://{S3_BUCKET}.s3.amazonaws.com/{s3_key}"
+            pet_profile_data_dict['image'] = image_url
+            print(f"Saving pet profile to DynamoDB: Table={DYNAMODB_TABLE_NAME}")
             table.put_item(Item=pet_profile_data_dict)
             return CreatePetProfile(pet=pet_profile_data_dict)
         except Exception as e:
@@ -109,7 +129,6 @@ def validate_input(input_data):
     ]
     return all(hasattr(input_data, field) for field in required_fields)
 
-# AWS Lambda handler
 def lambda_handler(event, context):
     print('Request event:', event)
     body = event.get('body', '{}')
